bcd56312c95d8c7b94e008431634e41cb271b768
bf0880-boop <bf0880@est.ort.edu.ar>
2025-08-11 10:42:01 -0300

    Hice crear cuenta y que se pueda escribir

--- Front end/Pantallaprincipal.js
+++ Front end/Pantallaprincipal.js
@@ -1,13 +1,13 @@
-let boton = document.querySelector(".rayasfiltro");
+let botonfiltros = document.querySelector(".rayasfiltro");
 let items = document.querySelectorAll(".menu-item");
 
-boton.addEventListener("click", () => {
+botonfiltros.addEventListener("click", () => {
     items.forEach(item => {
         item.classList.toggle("show");
     });
 });
 document.addEventListener("click", (e) => {
-    if (!boton.contains(e.target) && !e.target.classList.contains("menu-item")) {
+    if (!botonfiltros.contains(e.target) && !e.target.classList.contains("menu-item")) {
         items.forEach(item => item.classList.remove("show"));
     }
 });
