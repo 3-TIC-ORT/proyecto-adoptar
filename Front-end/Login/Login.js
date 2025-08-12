d17c0d08163a25b8c756833b346fc550b536ed5d
bf0880-boop <bf0880@est.ort.edu.ar>
2025-08-12 15:44:49 -0300

    Linkee la pantalla porincipal con todo. Despues sigo linkeando

---
+++ Front-end/Login/Login.js
@@ -0,0 +1,4 @@
+let botonsesion= document.getElementById("Iniciar-sesion");
+botonsesion.addEventListener("click", () => {
+window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Pantallaprincipal/Pantallaprincipal.html";
+});
--- Front-end/Pantallaprincipal/Pantallaprincipal.js
+++ Front-end/Pantallaprincipal/Pantallaprincipal.js
@@ -20,3 +20,27 @@ let botonformulario= document.querySelector(".circulo");
 botonformulario.addEventListener("click", () => {
 window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Formulario/Formulario.html";
 });
+let iradoptar= document.getElementById("Paraadoptar");
+iradoptar.addEventListener("click", () => {
+    window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Paraadoptar/Paraadoptar.html";
+    });
+    let irtransitar= document.getElementById("Paratransitar");
+irtransitar.addEventListener("click", () => {
+    window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Paratransitar/Paratransitar.html";
+    });
+    let irperdidos= document.getElementById("Perdidos");
+    irperdidos.addEventListener("click", () => {
+        window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Perdidos/Perdidos.html";
+        });
+    let irencontrados= document.getElementById("Encontrados");
+irencontrados.addEventListener("click", () => {
+    window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Encontrados/Encontrados.html";
+         });
+      let irmispublicaciones= document.getElementById("Mispublicaciones");
+     irmispublicaciones.addEventListener("click", () => {
+    window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Mispublicaciones/Mispublicaciones.html";
+     });
+ let irmisfavoritos= document.getElementById("Misfavoritos");
+    irmisfavoritos.addEventListener("click", () => {
+    window.location.href = "file:///C:/Users/50029728/Documents/GitHub/proyecto-adoptar/Front-end/Misfavoritos/Misfavoritos.html";
+    });
