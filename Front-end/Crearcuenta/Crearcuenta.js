bef924fb7064b9d8ee5f7ce447bc9e2d7297a5f7
bf0880-boop <bf0880@est.ort.edu.ar>
2025-08-16 18:04:59 -0300

    Linkeé el crear y login (porque se me habia ido) y cambié algo de la transición

--- Front-end/Crearcuenta/Crearcuenta.js
+++ Front-end/Crearcuenta/Crearcuenta.js
@@ -2,3 +2,7 @@ let botoncrearcuenta = document.getElementById("Crear-cuenta");
 botoncrearcuenta.addEventListener("click", () => {
 window.location.href = "../Pantallaprincipal/Pantallaprincipal.html";
 });
+let iralogin= document.getElementById("Sicuenta");
+iralogin.addEventListener("click", () => {
+window.location.href = "../Login/Login.html";
+});
--- Front-end/Login/Login.js
+++ Front-end/Login/Login.js
@@ -2,3 +2,7 @@ let botonsesion= document.getElementById("Iniciar-sesion");
 botonsesion.addEventListener("click", () => {
 window.location.href = "../Pantallaprincipal/Pantallaprincipal.html";
 });
+let iracrear= document.getElementById("Nocuenta");
+iracrear.addEventListener("click", () => {
+window.location.href = "../Crearcuenta/Crearcuenta.html";
+});
