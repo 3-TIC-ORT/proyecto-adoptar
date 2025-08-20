8ffeefbda36ec72327e4d9e8ec3f3de8cbb009f6
echununer <echununerpro@gmail.com>
2025-08-20 11:56:15 -0300

    Arranque con el "test drive"

---
+++ Back-end/pruebajson.js
@@ -0,0 +1,21 @@
+let usuarios = 
+[
+    {
+        "nombre": "Pepito",
+        "apellido": "Gutierrez",
+        "mail": "pepito@gmail.com",
+        "contra": "1234"
+    },
+    
+    {
+        "nombre": "Pepita",
+        "apellido": "Gutierrez",
+        "mail": "pepita@gmail.com",
+        "contra": "1234"
+    }
+]
+
+let nombre = prompt("Ingresar tu nombre")
+let apellido= prompt ("Imgresar tu apellido")
+let mail= prompt("Ingresar tu mail")
+let contra= prompt ("Ingresar tu contraseña")
