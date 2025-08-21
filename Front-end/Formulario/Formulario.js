7baff01335729cdb53a41a22795c995a8b4ccd9a
RamiroLangsam <ramirolangsam@gmail.com>
2025-08-21 10:22:49 -0300

    Mejoramos el formulario y cambiamos color fondo temporalmente de pp y del formulario

---
+++ Front-end/Formulario/Formulario.js
@@ -0,0 +1,4 @@
+let irapaginaprincipal = document.querySelector("#foto3");
+irapaginaprincipal.addEventListener("click", () => {
+window.location.href = "../Pantallaprincipal/Pantallaprincipal.html";
+});
